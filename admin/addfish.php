0f62d2a6d8bf5c592c941804a6c52daf7932af54
K <khoadh28@gmail.com>
2020-10-07 14:22:56 +0700

    quick fix

--- admin/addfish.php
+++ admin/addfish.php
@@ -40,7 +40,7 @@ if (isset($_POST['submit'])) {
     
         $target_path = "../uploads/$fishscientificname"; //Declaring Path for uploaded images
            for ($i = 0; $i < count($_FILES['file']['name']); $i++) {//loop to get individual element from the array
-       
+                $target_path = "../uploads/$fishscientificname";
                $validextensions = array("jpeg", "jpg", "png");  //Extensions which are allowed
                $ext = explode('.', basename($_FILES['file']['name'][$i]));//explode file name from dot(.) 
                $file_extension = end($ext); //store extensions in the variable
@@ -53,47 +53,7 @@ if (isset($_POST['submit'])) {
           if (($_FILES["file"]["size"][$i] < 1000000) //Approx. 1000mb files can be uploaded.
                        && in_array($file_extension, $validextensions)) {
                    if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {//if file moved to uploads folder
-                        $fp = fopen("../uploads/$fishscientificname/$fishscientificname.txt","w");
-                        if ($fp == true)
-                        {
-                            if (fwrite($fp,$fishdetail))
-                            {
-                                $fish = new fish();
-                                $fish->setfishname($fishname);
-                                $fish->setfishscientificname($fishscientificname);
-                                $fish->sethabitat($habitat);
-                                $fish->setdiet($diet);
-                                $fish->setsize($size);
-                                $fish->setweight($weight);
-                                $fish->setgestationperiod($gestationperiod);
-                                $fish->setachievableage($achievableage);
-                                $fish->setlocationid($locationid);
-                                $fish->settypeid($typeid);
-                                $fish->setstatus($status);
-                                $fish->setfishstatus(1);
-                                $fish->setfishdetail($fishscientificname.".txt");
-                                if(Create($fish))
-                                {
-                                    includeWithVariables("popup.php",array('title'=>'Sucessfully Added','color'=>'#dc3545','content' =>$fishscientificname.' has been added to database'));
-
-                                }
-                                else
-                                {
-                                    includeWithVariables("popup.php",array('title'=>'Failed To Added','color'=>'#dc3545','content' =>$fishscientificname.' has not been added to database'));
-
-                                }
-
-                            }
-                            else
-                            {
-                                includeWithVariables("popup.php",array('title'=>'Failed To Add','color'=>'#dc3545','content' =>'Please try again later!'));
- 
-                            }
-                        }
-                        else{
-                            includeWithVariables("popup.php",array('title'=>'Failed To Add','color'=>'#dc3545','content' =>'Please try again later!'));
-
-                        }
+                   
                         
                    } else {//if file was not moved.
                        includeWithVariables("popup.php",array('title'=>'Failed To upload','color'=>'#dc3545','content' =>'Please check your images again!'));
@@ -104,6 +64,47 @@ if (isset($_POST['submit'])) {
                    
        
                }
+           }
+           $fp = fopen("../uploads/$fishscientificname/$fishscientificname.txt","w");
+           if ($fp == true)
+           {
+               if (fwrite($fp,$fishdetail))
+               {
+                   $fish = new fish();
+                   $fish->setfishname($fishname);
+                   $fish->setfishscientificname($fishscientificname);
+                   $fish->sethabitat($habitat);
+                   $fish->setdiet($diet);
+                   $fish->setsize($size);
+                   $fish->setweight($weight);
+                   $fish->setgestationperiod($gestationperiod);
+                   $fish->setachievableage($achievableage);
+                   $fish->setlocationid($locationid);
+                   $fish->settypeid($typeid);
+                   $fish->setstatus($status);
+                   $fish->setfishstatus(1);
+                   $fish->setfishdetail($fishscientificname.".txt");
+                   if(Create($fish))
+                   {
+                       includeWithVariables("popup.php",array('title'=>'Sucessfully Added','color'=>'#dc3545','content' =>$fishscientificname.' has been added to database'));
+
+                   }
+                   else
+                   {
+                       includeWithVariables("popup.php",array('title'=>'Failed To Added','color'=>'#dc3545','content' =>$fishscientificname.' has not been added to database'));
+
+                   }
+
+               }
+               else
+               {
+                   includeWithVariables("popup.php",array('title'=>'Failed To Add','color'=>'#dc3545','content' =>'Please try again later!'));
+
+               }
+           }
+           else{
+               includeWithVariables("popup.php",array('title'=>'Failed To Add','color'=>'#dc3545','content' =>'Please try again later!'));
+
            }
     }
     else
@@ -165,14 +166,14 @@ if (isset($_POST['submit'])) {
                     <div class="row">
                         <div class="form-group col">
                             <label for="size">Size</label>
-                            <input type="number" min="0" type="size" class="form-control" id=""
-                                aria-describedby="" placeholder="Enter size (cm)" name="size" required>
+                            <input type="number" min="0" type="size" class="form-control" id="" aria-describedby=""
+                                placeholder="Enter size (cm)" name="size" required>
                         </div>
 
                         <div class="form-group col">
                             <label for="weight">Weight</label>
-                            <input type="number" min="0" type="weight" class="form-control" id=""
-                                aria-describedby="" placeholder="Enter weight (g)" name="weight" required>
+                            <input type="number" min="0" type="weight" class="form-control" id="" aria-describedby=""
+                                placeholder="Enter weight (g)" name="weight" required>
                         </div>
                     </div>
                     <div class="row">
@@ -206,7 +207,7 @@ if (isset($_POST['submit'])) {
                     </div>
                     <div class="form-group">
                         <label for="images">Fish Images</label>
-                        <div id="filediv"><input name="file[]" type="file" id="file" multiple/></div>
+                        <div id="filediv"><input name="file[]" type="file" id="file" multiple /></div>
                     </div>
                     <div class="form-group">
                         <label for="fishdetail">Fish Detail</label>
